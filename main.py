8c91ec4e60b6309726a00473967cb9bc4b61137a
Aayush Chadha <achadharocks@gmail.com>
2019-02-06 12:31:49 +0000

    Add transformation function calls

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-from utils import LoadRawImageData
+from utils import LoadRawImageData, DiskWriteUtil, TransformImages
 
 unzipped = LoadRawImageData.uncompressFile("data_pipeline/train-images-idx3-ubyte.gz")
 
 images = LoadRawImageData.extractRawData(unzipped)
 
-LoadRawImageData.writeDataToFile(images)
+DiskWriteUtil.writeDataToFile(images, "rawImages", "raw")
+
+transformedImages = TransformImages.applyTransformations(images, 2)
+
+DiskWriteUtil.writeDataToFile(transformedImages, "transformedImages", "transformed")
 
 
