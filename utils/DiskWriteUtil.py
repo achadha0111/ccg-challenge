910f035584123dc2bfc70dd8f553fa96489f3ab5
Aayush Chadha <achadharocks@gmail.com>
2019-02-06 12:32:19 +0000

    Refactor image write function to be used across modules

---
+++ utils/DiskWriteUtil.py
@@ -0,0 +1,13 @@
+from os import path, makedirs
+import imageio
+
+def writeDataToFile(imageData, directory, description):
+
+    if not path.exists(directory):
+        makedirs(directory)
+
+    # TODO Optimise this
+
+    for idx, file in enumerate(imageData):
+        fileName = "{0}_{1}.png".format(description, idx)
+        imageio.imwrite(path.join(description, fileName), file)
