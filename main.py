344bfe022cdba9bb777babbb405d7d611d980533
Aayush Chadha <achadharocks@gmail.com>
2019-02-06 13:28:43 +0000

    Add call to noise generating function

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
-from utils import LoadRawImageData, DiskWriteUtil, TransformImages
+from utils import LoadRawImageData, DiskWriteUtil, TransformImages, Noisy
+
+# TODO Add argparser to allow command line execution
 
 unzipped = LoadRawImageData.uncompressFile("data_pipeline/train-images-idx3-ubyte.gz")
 
 images = LoadRawImageData.extractRawData(unzipped)
 
-DiskWriteUtil.writeDataToFile(images, "rawImages", "raw")
+#DiskWriteUtil.writeDataToFile(images, "rawImages", "raw")
 
 transformedImages = TransformImages.applyTransformations(images, 2)
 
 DiskWriteUtil.writeDataToFile(transformedImages, "transformedImages", "transformed")
+
+noisyImages = Noisy.createNoisyImage(transformedImages, 2)
+
+DiskWriteUtil.writeDataToFile(transformedImages, "noisyImages", "transformedNoisy")
 
 
